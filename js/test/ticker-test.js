d5349b342fee1756234d35a632e3465f54586e97
Sudheer Apte <sapte@rethinkrobotics.com>
2018-09-07 09:16:05 -0400

    construct two-tier tree in ticker-test

--- js/test/ticker-test.js
+++ js/test/ticker-test.js
@@ -10,9 +10,35 @@ if (arr.length !== 1) {
 }
 // console.log(`arr = ${JSON.stringify(arr)}`);
 
+/**
+   construct:
+     1
+     +-2,     3
+       +-4,6  +-5
+*/
+
+const parents = {2: 1, 3: 1, 4: 2, 6: 2, 5: 3};
+
+console.log(`adding child 2`);
+const two = ticker.addNode('Parallel', 1, 0);
+console.log(`two = ${two}`);
+err(two === 2);
+console.log(`adding child 3`);
+const three = ticker.addNode('Parallel', 1, 0);
+err(three === 3);
 arr = [];
-const n = ticker.addNode('Simple', 1, 0);
 ticker.traverseAll(1, arr);
-if (arr.length !== 2) {
-  err(`arr = ${JSON.stringify(arr)}`);
+console.log(`arr = ${JSON.stringify(arr)}`);
+
+
+for (let i=4; i<7; i++) {
+  console.log(`adding child ${i} to ${parents[i]}`);
+  const n = ticker.addNode('Simple', parents[i], 0);
+  arr = [];
+  ticker.traverseAll(1, arr);
+  console.log(`arr = ${JSON.stringify(arr)}`);
+  if (arr.length !== i) {
+    err(`arr = ${JSON.stringify(arr)}`);
+  }
 }
+
